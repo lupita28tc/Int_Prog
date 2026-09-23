f19306bbf4ba7ce324432b285ca4d5ced49725c3
Miriam Trejo <lupitatrejo28tc@gmail.com>
2026-09-22 22:48:11 -0600

    CREATE

---
+++ Ejercicios_Clase_21/09/26/Calculadora.py
@@ -0,0 +1,24 @@
+a=float(input("Ingrese a:"))
+b=float(input("Ingrese b:"))
+op=input("Operación (+,-,*,/):")
+
+match op:
+    case "+":
+        resultado= a + b
+        print("Resultado:",resultado)
+    case "-":
+        resultado = a - b
+        print("Resultado:",resultado)
+    case "*":
+        resultado=a * b
+        print("Resultado:",resultado)
+    case "/":
+        if b==0:
+           print("Error: no se puede dividir entre cero.")
+        else:
+          resultado= a / b
+          print("Resultado:",resultado)
+          
+    case _:
+        print("Operación no válida.")
+        
