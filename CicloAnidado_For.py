4fd108f43d86b4632cc3c28325d9cb613eac08fe
lupita28tc <lupitatrejo28tc@gmail.com>
2026-09-24 11:57:46 -0600

    Delete CicloAnidado_For.py

--- CicloAnidado_For.py
+++
@@ -1,9 +0,0 @@
-inicio_ciclo_exterior = int(input("Dame el inicio del ciclo exterior: "))
-fin_ciclo_exterior = int(input("Dame el fin del ciclo exterior: "))
-inicio_ciclo_interior = int(input("Dame el inicio del ciclo interior: "))
-fin_ciclo_interior = int(input("Dame el fin del ciclo interior: "))
-
-for i in range(inicio_ciclo_exterior, fin_ciclo_exterior + 1):
-    print(f"T A B L A = {i}")
-    for j in range(inicio_ciclo_interior, fin_ciclo_interior + 1):
-        print(f"{i} X {j} = {i * j}")
